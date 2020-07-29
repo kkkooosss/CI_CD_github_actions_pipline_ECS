7d99c301ba95e616df7d4bc76b1d2b8848d60c7d
EC2 Default User <ec2-user@ip-172-31-4-182.eu-central-1.compute.internal>
2020-07-29 11:33:01 +0000

    new flask app

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-"""Main application file"""
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
-@app.route('/<random_string>')
-def returnBackwardsString(random_string):
-    """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+application = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+
+@application.route("/")
+def root():
+    return render_template("index.html")
+
+@application.route("/help")
+def helppage():
+    return render_template("help.html")
+
+#--------Main------------------
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+#------------------------------
